f48376fc0a4f8dd2bbae3adbe9e19bf49d51384e
Eduárd Pocstár <admdh.com@gmail.com>
2016-07-16 11:39:14 +0200

    console.log update

--- app-source/console-log.js
+++ app-source/console-log.js
@@ -1,5 +1,7 @@
 module.exports = (name) => {
 	console.log('\n---------------------------------------------------------');
-	console.log(name + ' is created!\nThank you for using ADM DEV KIT!');
+	console.log(name + ' is created!\nThank You for using ADM DEV KIT!');
+	console.log('---------------------------------------------------------');
+	console.log('Don\'t forget to run npm i and jspm i');
 	console.log('---------------------------------------------------------\n');
 };
