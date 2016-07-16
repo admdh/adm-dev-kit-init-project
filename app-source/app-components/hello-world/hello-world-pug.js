c5f3fe7f10b738599314ce66352a69f6cf6605f9
Eduárd Pocstár <admdh.com@gmail.com>
2016-07-16 23:08:54 +0200

    minor fixes + adm-dev-kit version update

--- app-source/app-components/hello-world/hello-world-pug.js
+++ app-source/app-components/hello-world/hello-world-pug.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra');
 
 module.exports = (source) => {
 	const fileContent = 'link(href=\'index/hello-world/_hello-world.css\' ' +
-'							type=\'text/css\' rel=\'stylesheet\')\n' +
+						'type=\'text/css\' rel=\'stylesheet\')\n' +
 		'.hello-world\n' +
 		'\th1 Hello, World!\n\n' +
 		'script System.import(\'index/hello-world/_hello-world.js\')';
