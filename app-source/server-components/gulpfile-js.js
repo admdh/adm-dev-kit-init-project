60b2fc586ee0c22063379b2834327a72a34a35e7
Eduárd Pocstár <admdh.com@gmail.com>
2016-08-05 12:36:32 +0200

    minor fixes

--- app-source/server-components/gulpfile-js.js
+++ app-source/server-components/gulpfile-js.js
@@ -4,8 +4,8 @@ module.exports = () => {
 	const fileContent = 'var app = require(\'adm-dev-kit\');\n' +
 		'var pjson = require(\'./package.json\');\n\n' +
 		'app.build({\n' +
-		'\tsrc: pjson.srcFolder,\n' +
-		'\tdest: pjson.buildFolder\n' +
+		'\tsrc: pjson.adm.srcFolder,\n' +
+		'\tdest: pjson.adm.buildFolder\n' +
 		'});\n';
 
 	fs.writeFile('gulpfile.js', fileContent, (err) => {
